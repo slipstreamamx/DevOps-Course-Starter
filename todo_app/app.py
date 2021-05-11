2c421f9167e2ca78c209d6287f845fe0ad5e3620
asad.x.omar@gmail.com <asad.x.omar@gmail.com>
2021-05-11 19:01:26 +0100

    first code change

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+from todo_app.data.session_items import *
 
 from todo_app.flask_config import Config
 
@@ -8,7 +9,7 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', all_items=get_items())
 
 
 if __name__ == '__main__':
