f426a00b12654ab0ac73b695c1a8bfe43a226666
asad.x.omar@gmail.com <asad.x.omar@gmail.com>
2021-05-11 19:40:20 +0100

    second task

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from flask.globals import request
+from werkzeug.utils import redirect
 from todo_app.data.session_items import *
 
 from todo_app.flask_config import Config
@@ -11,6 +13,14 @@ app.config.from_object(Config)
 def index():
     return render_template('index.html', all_items=get_items())
 
+@app.route('/addItem', methods=['GET', 'POST'])
+def addItem():
+    if request.method == 'POST':
+        title = request.form.get('inputItem')
+        add_item(title)
+        return redirect('/')
+    else:
+        return redirect('/')
 
 if __name__ == '__main__':
     app.run()
