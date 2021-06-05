276f65c3ccbad1306781c5f0807241af691ee634
asad.x.omar@gmail.com <asad.x.omar@gmail.com>
2021-06-05 19:05:35 +0100

    Module 2

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -5,9 +5,9 @@ class Config:
     """Base configuration variables."""
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
-    TRELLO_API_KEY='0e8c5313a51ec05165d5846ccdb0b984'
-    TRELLO_API_SECRET='02436356bfd15cc68db990ef94cc1f07ac2eca4526a7c3bc672201a588ea7879'
-    TRELLO_BOARD_ID='60a21e48ae8d9a3a66965e3e'
+    TRELLO_API_KEY=
+    TRELLO_API_SECRET=
+    TRELLO_BOARD_ID=
 
     TRELLO_BASE_URL = 'https://api.trello.com/1'
 
